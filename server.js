3e5f8a2f438d251f5c523668e0b2a921122e6d9e
kachitra <tofcomputation@gmail.com>
2017-03-30 11:28:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 })
 app.get('/article1', function (req, res) {
-  res.sendFile('article1 here');
+  res.send('article1 here');
 });
 
 app.get('/ui/style.css', function (req, res) {
