f500b71d2090b22796d5c33d51a5b5a543b54de3
kachitra <tofcomputation@gmail.com>
2017-03-30 11:29:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-})
+});
 app.get('/article1', function (req, res) {
   res.send('article1 here');
 });
